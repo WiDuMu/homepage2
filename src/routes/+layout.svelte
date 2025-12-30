3834eba0f1d4dd759e324e8e1559e295369b4ae0
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-12-29 20:46:37 -0600

    get to old version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+import "./layout.css";
+let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children()}
+<main class="min-h-screen w-screen flex flex-row font-display justify-center">
+<article class="m-y-1 prose prose-slate dark:prose-invert justify-center w-5/6 p-4 bg-blend-color bg-[#77777777]">
+   {@render children()}
+</article>
+</main>
